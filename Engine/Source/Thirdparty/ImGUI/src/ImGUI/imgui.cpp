25e92d047343eede1233e5f51e08a607742a782c
Daniel Assuncao <dinho9.epaeh@hotmail.com>
2018-08-11 02:32:44 +0100

    Changed so it saves to the same file on imgui

--- Engine/Source/Thirdparty/ImGUI/src/ImGUI/imgui.cpp
+++ Engine/Source/Thirdparty/ImGUI/src/ImGUI/imgui.cpp
@@ -4254,12 +4254,14 @@ void ImGui::SaveIniSettingsToDisk(const char* ini_filename)
 {
     ImGuiContext& g = *GImGui;
     g.SettingsDirtyTimer = 0.0f;
-    if (!ini_filename)
-        return;
 
     size_t ini_data_size = 0;
     const char* ini_data = SaveIniSettingsToMemory(&ini_data_size);
-    FILE* f = ImFileOpen(ini_filename, "wt");
+
+	SKString dir = Skel::FileSystem::GetEnginePath();
+	dir.append("\\imgui.ini");
+
+    FILE* f = ImFileOpen(dir.c_str(), "wt");
     if (!f)
         return;
     fwrite(ini_data, sizeof(char), ini_data_size, f);
